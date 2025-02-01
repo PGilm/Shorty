34b0eb5154883214ba80957954ddce2a0d812736
PGilm <PhilipGilman@gmail.com>
2025-02-01 00:31:44 -0500

    Refactor Flask app initialization and update CSS for improved layout and overflow handling

--- App/app.py
+++ App/app.py
@@ -10,7 +10,6 @@ import pandas as pd
 app = Flask(__name__)
 app.secret_key = "123456789"
 
-app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
