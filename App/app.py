1192f595a849863a65eadcbb44d6a229434b2352
PGilm <PhilipGilman@gmail.com>
2025-02-06 15:19:48 -0500

    -250206pg
    prod

--- App/app.py
+++ App/app.py
@@ -158,7 +158,7 @@ def oldhello_there(name):
 if __name__ == '__main__':
     app.run(
         debug=True,
-        # host='0.0.0.0',
-        # port=5001,
+        host='0.0.0.0',
+        port=5001,
         )
     
