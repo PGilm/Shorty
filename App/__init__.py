1c64eb3d6521a82c0a57f862d62f5e12edeed395
PGilm <PhilipGilman@gmail.com>
2025-03-20 23:22:51 -0400

    Implement session management with Flask-Session; configure session lifetime and secret key

--- App/__init__.py
+++ App/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 
 from flask_login import LoginManager # Add this line
+from datetime import timedelta
 
 app = Flask(__name__)
 app.config.from_object(config("APP_SETTINGS"))
@@ -14,6 +16,14 @@ login_manager.init_app(app) # Add this line
 
 login_manager.login_view = "accounts.login"
 login_manager.login_message_category = "danger"
+
+app.secret_key = config("SECRET_KEY")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+app.permanent_session_lifetime = timedelta(days=15)
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
